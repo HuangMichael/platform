03cb9576b762cf9ee47b1eb3c6a2a33486c613e7
huangbin <876301469@qq.com>
2018-05-24 08:35:42 +0800

    c

--- src/main/java/com/subway/userLog/UserLogService.java
+++ src/main/java/com/subway/userLog/UserLogService.java
@@ -1,12 +1,11 @@
 package com.subway.userLog;
 
-import com.subway.object.ReturnObject;
 import com.subway.service.app.BaseService;
 import com.subway.service.commonData.CommonDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import static com.subway.utils.ConstantUtils.*;
+import java.util.Date;
 
 
 /**
@@ -20,47 +19,21 @@ import static com.subway.utils.ConstantUtils.*;
 public class UserLogService extends BaseService {
 
 
-    @Autowired(required = false)
-    UserLogRepository userLogRepository;
 
 
     @Autowired
     CommonDataService commonDataService;
 
 
-    /**
-     * @param id id
-     * @return 根据id删除对象
-     */
-    public ReturnObject delete(Long id) {
-        userLogRepository.delete(id);
-        UserLog userLog = userLogRepository.findOne(id);
-        return commonDataService.getReturnType(userLog == null, DELETE_SUCCESS, DELETE_FAILURE);
-    }
-
-
-    /**
-     * @param userLog
-     * @return 保存信息
-     */
-    public ReturnObject save(UserLog userLog) {
-
-        userLog = userLogRepository.save(userLog);
-        return commonDataService.getReturnType(userLog != null, SAVE_SUCCESS, SAVE_FAILURE);
-    }
-
 
     public UserLog findById(Long id) {
-        return userLogRepository.getOne(id);
+        return null ;
     }
 
 
-    /**
-     * @param userLog
-     * @return 创建用户日志
-     */
     public UserLog createUserLog(UserLog userLog) {
-        return userLogRepository.save(userLog);
+        log.info(new Date() + "userLog----------------" + userLog.toString());
+        return null;
     }
 
 }
